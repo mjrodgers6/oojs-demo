29bdb1b9447fb23e390c82d050fc7a04c5138cff
Jamie Rodgers <mjrodgers6@gmail.com>
2015-06-18 16:26:30 -0400

    Added HW

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-function Ninja(stars){
+//Class Example
+/*function Ninja(stars){
 
   this.stars = stars;
 
@@ -28,9 +29,37 @@ ninja2.hasMask = true;
 Ninja.prototype.wearsBlack = true;
 
 function MountedNinja(){
+  console.log(this);
+  console.log(arguments);
+  
+  Ninja.apply(this, arguments);
   this.ridesHorse = true;
 }
 
 MountedNinja.prototype = new Ninja(4);
 
-rider = new MountedNinja();
+rider = new MountedNinja(); */
+
+function Anchor(age, gender, signoff){
+    this.age = age;
+    this.gender = gender;   
+}
+
+function National() {
+    Anchor.apply(this, arguments);
+    this.signoff = "Good Night and Good Luck."    
+}
+
+function Local() {
+    Anchor.apply(this, arguments);
+    this.signoff = "Stay Classy, San Diego"
+}
+
+National.prototype = new Anchor();
+Local.prototype = new Anchor();
+
+var lester = new National(56, "male");
+var katie = new National(58, "female");
+
+var burgundy = new Local(47, "male");
+var corningstone = new Local(43, "female");
